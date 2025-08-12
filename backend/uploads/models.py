bac9efb4825d7c5ef264b55e97c1fd82da1502e1
Jeff Culbert <jeff.culbert@gmail.com>
2025-08-12 14:20:18 -0400

    Increased upload filename length limit from 50 to 200

--- backend/uploads/models.py
+++ backend/uploads/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Upload(models.Model):
     date = models.DateField(auto_now_add=True)
-    file_name = models.TextField(max_length=50, null=True)
+    file_name = models.TextField(max_length=100, null=True)
     num_transactions = models.IntegerField()
     result = models.TextField(max_length=200)
     notes = models.TextField(max_length=50000, null=True)
